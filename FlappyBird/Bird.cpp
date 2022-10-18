a728169d3240f7bd8f96dc454e48be224f19b8f4
KPhat <keiryphat@gmail.com>
2022-10-18 13:29:18 +0700

    bird animation

--- FlappyBird/Bird.cpp
+++ FlappyBird/Bird.cpp
@@ -1,5 +1,72 @@
 #include "Bird.h"
-
+#include "GameLoop.h"
+using namespace std;
 void Bird::Render(SDL_Renderer* ren) {
-	SDL_RenderCopy(ren, getTex(), &getScrDimension(), &getDesDimension());
+	timeAnimation++;
+	if (timeAnimation < 20) {
+		SDL_RenderCopyEx(ren, getTex(), &getScrDimension(), &getDesDimension(), 0, NULL, SDL_FLIP_NONE);
+	}
+	else if (timeAnimation >= 20 && timeAnimation <= 40) {
+		SDL_RenderCopyEx(ren, tex1, &getScrDimension(), &getDesDimension(), 0, NULL, SDL_FLIP_NONE);
+	}
+	else if (timeAnimation > 40) {
+		SDL_RenderCopyEx(ren, tex2, &getScrDimension(), &getDesDimension(), 0, NULL, SDL_FLIP_NONE);
+	}
+	if (timeAnimation > 60) {
+		timeAnimation = 0;
+	}
+}
+
+
+void Bird::Gravity() {
+	if (getJumpState()) {
+		jumpHeight = gravity + jumpHeight;
+		accelerator1 = accelerator1 + 0.035;
+		accelerator2 = accelerator2 + 0.035;
+		bird_position = bird_position + gravity + accelerator1 + accelerator2 + jumpHeight;
+		setDesDimension(50, bird_position, 34, 24);
+		if (jumpHeight > 0) {
+			inJump = false;
+			jumpHeight = -7;
+		}
+	}
+	else {
+		accelerator1 = accelerator1 + 0.035;
+		accelerator2 = accelerator2 + 0.035;
+		bird_position = bird_position + gravity + accelerator1 + accelerator2;
+		setDesDimension(50, bird_position, 34, 24);
+	}
+}
+
+double Bird::getBirdPosition() {
+	return bird_position;
+}
+
+void Bird::getJumpTime() {
+	jumptimer = SDL_GetTicks();
+}
+
+bool Bird::getJumpState() {
+	return inJump;
+}
+
+void Bird::Jump() {
+	if (jumptimer - lastJump > 100) {
+		accelerator1 = 0;
+		accelerator2 = 0;
+		inJump = true;
+		lastJump = jumptimer;
+	}
+	else
+	{
+		Gravity();
+	}
+}
+
+void Bird::CreateTexture1(const char* filelocation, SDL_Renderer* ren) {
+	tex1 = TextureManager::Texture(filelocation, ren);
+}
+
+void Bird::CreateTexture2(const char* filelocation, SDL_Renderer* ren) {
+	tex2 = TextureManager::Texture(filelocation, ren);
 }
--- FlappyBird/Bird.h
+++ FlappyBird/Bird.h
@@ -3,6 +3,25 @@
 
 class Bird : public Entity {
 public:
-private:
 	void Render(SDL_Renderer* ren);
+	void Gravity();
+	double getBirdPosition();
+	void getJumpTime();
+	void Jump();
+	bool getJumpState();
+	void CreateTexture1(const char* filelocation, SDL_Renderer* ren);
+	void CreateTexture2(const char* filelocation, SDL_Renderer* ren);
+private:
+	int timeAnimation = 0;
+	double gravity = 0.2;
+	double bird_position = 50;
+	double jumpHeight = -7;
+	double accelerator1 = 0;
+	double accelerator2 = 0;
+	bool inJump = false;
+	double jumptimer;
+	double lastJump = 0;
+	SDL_Event jump;
+	SDL_Texture* tex1;
+	SDL_Texture* tex2;
 };
