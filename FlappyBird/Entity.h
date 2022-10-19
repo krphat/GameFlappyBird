a26404e003697e8c3f6fa884520e836948e12671
7iendat <datnguyentien0803@gmail.com>
2022-10-19 20:44:47 +0700

    update

--- FlappyBird/Entity.h
+++ FlappyBird/Entity.h
@@ -6,10 +6,7 @@ public:
 	void setScrDimension(int x, int y, int w, int h);
 	SDL_Rect& getDesDimension();
 	void setDesDimension(int x, int y, int w, int h);
-	int getX();
-	void setX(int x);
-	int getY();
-	void setY(int y);
+	
 	SDL_Texture* getTex();
 	void CreateTexture(const char* filelocation, SDL_Renderer* ren);
 	virtual void Render(SDL_Renderer* ren) = 0;
